16cf6164786731383439e54bd218a284fae9894d
Ivan Petrov <petivanv2005@gmail.com>
2025-12-24 23:08:17 +0300

    UI 1.0
    Первая версия UI

--- api.py
+++ api.py
@@ -1,16 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from recorder import Recorder
 from storage import MemoryStorage
 from battery import Battery
 from power_manager import PowerManager
 
 app = Flask(__name__)
+from flask_cors import CORS
+CORS(app)
 
 memory = MemoryStorage()
 battery = Battery()
 power = PowerManager(battery)
 recorder = Recorder(memory, battery, power)
 
+@app.route('/')
+def serve_index():
+    return send_from_directory('UI', 'index.html')
 
 @app.get("/messages")
 def get_messages():
